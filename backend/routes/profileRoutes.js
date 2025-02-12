e02c8798e86c9af3a5411a54aae3ffcab71dc316
Jahnavi23-lab <gm.jahnavi2002@gmail.com>
2025-02-12 12:00:39 +0530

    Delete backend directory

--- backend/routes/profileRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const profileController = require('../controllers/profileController');
-
-router.get('/:customer_id', profileController.getProfile);
-router.put('/:customer_id', profileController.updateProfile);
-
-module.exports = router;
